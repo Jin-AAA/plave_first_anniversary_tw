d0084c4d68250e6d7fc06c2ed40595e1d11cc0f6
Anny <eszwaq951@gmail.com>
2024-02-13 15:14:51 +0800

    test

---
+++ stg/app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/*路徑*/
+define('Document_root',dirname(__FILE__)            );
+define('WEB_ROOT'	, dirname(__FILE__).'/../'		);/*不用改*/
+define('WEB_INC'	, WEB_ROOT.'include_files/'		);/*include*/
+define('WEB_HOST'	, 'https://plave0312.tw/stg'		);/*網址*/
+define('WEB_PATH'	, '/'	);/*根目錄*/
+define('WEB_URL'	, WEB_HOST.WEB_PATH				);
+
+
+
+?>
