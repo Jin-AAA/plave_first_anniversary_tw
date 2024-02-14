234bf4efeff70b668ad1bc1c91aa071cae2df154
Anny <eszwaq951@gmail.com>
2024-02-14 18:27:18 +0800

    up

--- stg/js/index.js
+++ stg/js/index.js
@@ -1,22 +1,31 @@
 $(function(){
 
-    $(".photo_slide").ticker({
+    // $(".photo_slide").ticker({
 
-        // item selector
-        item: 'img',
+    //     // item selector
+    //     item: 'img',
       
-        // Toggles whether the ticker should pause on mouse hover
-        pauseOnHover: false,
+    //     // Toggles whether the ticker should pause on mouse hover
+    //     pauseOnHover: false,
       
-        // <a href="https://www.jqueryscript.net/animation/">Animation</a> speed
-        speed: 30,
+    //     // <a href="https://www.jqueryscript.net/animation/">Animation</a> speed
+    //     speed: 30,
       
-        // Decides whether the ticker breaks when it hits a new item or if the track has reset
-        pauseAt: '',
+    //     // Decides whether the ticker breaks when it hits a new item or if the track has reset
+    //     pauseAt: '',
       
-        // delay in milliseconds
-        //delay: 500
+    //     // delay in milliseconds
+    //     //delay: 500
       
-    });
+    // });
+
+    var elements = $('ul.tithome li').length;
+
+    for(var i=0;i < elements; i++){
+     $(".tithome").clone().prependTo( ".scorri" );
+    };
+    
+    var liEle = [];
+    var liEle = $(".tithome li");
 
 })
