31f5152e57555002d22910dc11549cb0e1dae8b5
Anny <eszwaq951@gmail.com>
2024-02-14 18:05:51 +0800

    0214 首頁進度

---
+++ stg/js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $(".photo_slide").ticker({
+
+        // item selector
+        item: 'img',
+      
+        // Toggles whether the ticker should pause on mouse hover
+        pauseOnHover: false,
+      
+        // <a href="https://www.jqueryscript.net/animation/">Animation</a> speed
+        speed: 30,
+      
+        // Decides whether the ticker breaks when it hits a new item or if the track has reset
+        pauseAt: '',
+      
+        // delay in milliseconds
+        //delay: 500
+      
+    });
+
+})
