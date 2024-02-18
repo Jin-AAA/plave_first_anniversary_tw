907da8fa41c4ecbc8ea2ad600fe4b20e60fa7261
Anny <eszwaq951@gmail.com>
2024-02-18 18:39:50 +0800

    首頁動畫

--- stg/js/index.js
+++ stg/js/index.js
@@ -1,24 +1,5 @@
 $(function(){
 
-    // $(".photo_slide").ticker({
-
-    //     // item selector
-    //     item: 'img',
-      
-    //     // Toggles whether the ticker should pause on mouse hover
-    //     pauseOnHover: false,
-      
-    //     // <a href="https://www.jqueryscript.net/animation/">Animation</a> speed
-    //     speed: 30,
-      
-    //     // Decides whether the ticker breaks when it hits a new item or if the track has reset
-    //     pauseAt: '',
-      
-    //     // delay in milliseconds
-    //     //delay: 500
-      
-    // });
-
     var elements = $('ul.tithome li').length;
 
     for(var i=0;i < elements; i++){
@@ -27,5 +8,10 @@ $(function(){
     
     var liEle = [];
     var liEle = $(".tithome li");
+
+    gsap.to(".slogan", {duration:2, autoAlpha:1, scale:1, ease: Power1.easeOut});
+    gsap.to(".cont", {duration:2, autoAlpha:1, ease: Power1.easeOut, delay:1});
+    gsap.to(".subti", {duration:2, autoAlpha:1, ease: Power1.easeOut, delay:1.5});
+    gsap.to(".nav", {duration:2, autoAlpha:1, ease: Power1.easeOut, delay:2});
 
 })
