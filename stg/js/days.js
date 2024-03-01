0d4600b290905e839555209bc2cd96834229a949
Anny <eszwaq951@gmail.com>
2024-03-02 01:29:55 +0800

    月曆設置

--- stg/js/days.js
+++ stg/js/days.js
@@ -1,78 +1,61 @@
-$(function(){
-    
-
-    // var calendarEl = document.getElementById('calendar');
-
-    // var calendar = new FullCalendar.Calendar(calendarEl, {
-    //     year: 2022,
-    //     month: 7,
-    //     height: 650,
-    //     locale: 'zh-tw',
-    //     timeZone: 'local',
-    //     //initialView: 'dayGridMonth',
-    //     events: 'https://fullcalendar.io/api/demo-feeds/events.json',
-    //     editable: false,
-    //     selectable: false
-    // });
-
-    // calendar.render();
-
-    //$("a.fc-event").attr("target", "_blank");
-
-})
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      height:700,
-      locale: 'zh-tw',
-      validRange: {
-        start: '2022-07-01',
-        end: '2024-03-31'
-      },
-      eventSources: [
-
-        // your event source
-        // cover
-        {
-          events: [ // put the array in the `events` property
-            {
-              title  : '💙第一首cover',
-              start  : '2022-07-28',
-              url: 'https://www.youtube.com/watch?v=zxAQ6YH3gAw'
+        eventClick: function(info) {
+            var eventObj = info.event;
+            if (eventObj.url) {
+              //alert(
+              //  'Clicked ' + eventObj.title + '.\n' +
+              //  'Will open ' + eventObj.url + ' in a new tab'
+              //);
+      
+              window.open(eventObj.url);
+      
+              info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+            } else {
+              //alert('Clicked ' + eventObj.title);
             }
-          ],
-          color: 'white',     // an option!
-          textColor: '#4563d3', // an option!
-          eventClick: function(info) {
-            info.jsEvent.preventDefault(); // don't let the browser navigate
-            if (info.event.url) {
-              window.open(info.event.url, "_blank");
-            }
-          }
         },
-        // LIVE
-        {
-            events: [ // put the array in the `events` property
-              {
-                title  : '💙第一次LIVE亮相',
-                start  : '2022-09-25',
-                url: 'https://www.youtube.com/watch?v=2l2nip8yy0g'
-              }
-            ],
-            color: '#4563d3',     // an option!
-            textColor: 'white', // an option!
-            eventClick: function(info) {
-              info.jsEvent.preventDefault(); // don't let the browser navigate
-              if (info.event.url) {
-                window.open(info.event.url, "_blank");
-              }
-            }
-        
-        }
-       ]
+        headerToolbar: {
+            left: 'dayGridMonth,dayGridWeek,listMonth', // buttons for switching between views
+            center: 'title'
+        },
+        //initialView: 'dayGridMonth',
+        initialDate: '2022-07-01',
+        height:'100%',
+        locale: 'zh-tw',
+        validRange: {
+            start: '2022-07-01',
+            end: '2024-03-31'
+        },
+        eventSources: [
+            // your event source
+            // cover
+            {
+                events: [ // put the array in the `events` property
+                    {
+                        title  : '💙第一首cover',
+                        start  : '2022-07-28',
+                        url: 'https://www.youtube.com/watch?v=zxAQ6YH3gAw'
+                    },
+                    {
+                        title  : '💙第一次LIVE亮相',
+                        start  : '2022-09-25',
+                        url: 'https://www.youtube.com/watch?v=2l2nip8yy0g'
+                    }
+                ],
+                color: 'white',     // an option!
+                borderColor: '#4563d3',
+                textColor: '#4563d3', // an option!
+                // eventClick: function(info) {
+                //     info.jsEvent.preventDefault(); // don't let the browser navigate
+                //     if (info.event.url) {
+                //         window.open(info.event.url, '_blank');
+                //     }
+                // }
+                
+            },
+        ]
     });
     calendar.render();
-  });
+});
