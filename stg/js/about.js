43fadd01ca5f58b73d8d5a3d022e834d651fff66
Anny <eszwaq951@gmail.com>
2024-03-08 02:15:46 +0800

    up

--- stg/js/about.js
+++ stg/js/about.js
@@ -9,4 +9,13 @@ $(function(){
     var liEle = [];
     var liEle = $(".tithome li");
 
+    var elements_b = $('ul.tithome_b li').length;
+
+    for(var i=0;i < elements_b; i++){
+     $(".tithome_b").clone().prependTo( ".scorri_b" );
+    };
+    
+    var liEle_b = [];
+    var liEle_b = $(".tithome_b li");
+
 })
